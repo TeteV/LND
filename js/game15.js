e5c16422c72e5289b8f6458f756348b575b03cf7
unknown <nestor.informatica95@gmail.com>
2019-11-14 17:33:16 +0000

    Third commit to add some difficult

--- js/game15.js
+++ js/game15.js
@@ -2,7 +2,13 @@ window.onload = initialize;
 
 function initialize(){
     butt = document.getElementById("buttonsin");
-    butt.addEventListener("click", addNumbers)
+    butt.addEventListener("click", addNumbers);
+
+    butt1 = document.getElementById("buttonsin1");
+    butt1.addEventListener("click", addNumbers1);
+
+    butt2 = document.getElementById("buttonsin2");
+    butt2.addEventListener("click", addNumbers2);
 }
 
 function addNumbers(){
@@ -31,9 +37,76 @@ function addNumbers(){
     if (e == 15) {
         document.getElementById("resulty2").style.display = "unset";
         document.getElementById("resulty1").style.display = "none";
+        document.getElementById("divcool").style.display = "unset";
       } else {
         document.getElementById("resulty1").style.display = "unset";
         document.getElementById("resulty2").style.display = "none";
+        document.getElementById("divcool").style.display = "none";
       }
 }
 
+function addNumbers1(){
+  var y,x,z;
+  a = document.getElementById("one1");
+  b = document.getElementById("two1");
+  c = document.getElementById("three1");
+  d = document.getElementById("res1");
+
+
+ y = Math.random() * (7 - 1) + 1;
+  var q = Math.round(y);
+ a.innerHTML = q;
+
+ x = Math.random() * (7 - 1) + 1;
+  var w = Math.round(x);
+ b.innerHTML = w;
+
+ z = Math.random() * (7 - 1) + 1;
+  var e = Math.round(z);
+ c.innerHTML = e;
+
+  d.innerHTML = q+w+e;
+  e = q+e+w;
+  
+  if (e == 10) {
+      document.getElementById("resulty21").style.display = "unset";
+      document.getElementById("resulty11").style.display = "none";
+      document.getElementById("divcool2").style.display = "unset";
+    } else {
+      document.getElementById("resulty11").style.display = "unset";
+      document.getElementById("resulty21").style.display = "none";
+      document.getElementById("divcool2").style.display = "none";
+    }
+}
+
+function addNumbers2(){
+  var y,x,z;
+  a = document.getElementById("one2");
+  b = document.getElementById("two2");
+  c = document.getElementById("three2");
+  d = document.getElementById("res2");
+
+
+ y = Math.random() * (7 - 1) + 1;
+  var q = Math.round(y);
+ a.innerHTML = q;
+
+ x = Math.random() * (7 - 1) + 1;
+  var w = Math.round(x);
+ b.innerHTML = w;
+
+ z = Math.random() * (7 - 1) + 1;
+  var e = Math.round(z);
+ c.innerHTML = e;
+
+  d.innerHTML = q+w+e;
+  e = q+e+w;
+  
+  if (e == 5) {
+      document.getElementById("resulty22").style.display = "unset";
+      document.getElementById("resulty12").style.display = "none";
+    } else {
+      document.getElementById("resulty12").style.display = "unset";
+      document.getElementById("resulty22").style.display = "none";
+    }
+}
